5befa4270c5aace06591f9609442040ed3d2a1ca
pankajguptagithub <pankajguptagithub@users.noreply.github.com>
2024-03-25 21:01:13 +0530

    grading_students.go

---
+++ hkrrnk/grading_students.go
@@ -0,0 +1,156 @@
+/*
+HackerLand University has the following grading policy:
+
+Every student receives a  in the inclusive range from  to .
+Any  less than  is a failing grade.
+Sam is a professor at the university and likes to round each student's  according to these rules:
+
+If the difference between the  and the next multiple of  is less than , round  up to the next multiple of .
+If the value of  is less than , no rounding occurs as the result will still be a failing grade.
+Examples
+
+ round to  (85 - 84 is less than 3)
+ do not round (result is less than 40)
+ do not round (60 - 57 is 3 or higher)
+Given the initial value of  for each of Sam's  students, write code to automate the rounding process.
+
+Function Description
+
+Complete the function gradingStudents in the editor below.
+
+gradingStudents has the following parameter(s):
+
+int grades[n]: the grades before rounding
+Returns
+
+int[n]: the grades after rounding as appropriate
+Input Format
+
+The first line contains a single integer, , the number of students.
+Each line  of the  subsequent lines contains a single integer, .
+
+Constraints
+
+Sample Input 0
+
+4
+73
+67
+38
+33
+Sample Output 0
+
+75
+67
+40
+33
+Explanation 0
+
+image
+
+Student  received a , and the next multiple of  from  is . Since , the student's grade is rounded to .
+Student  received a , and the next multiple of  from  is . Since , the grade will not be modified and the student's final grade is .
+Student  received a , and the next multiple of  from  is . Since , the student's grade will be rounded to .
+Student  received a grade below , so the grade will not be modified and the student's final grade is .
+Language
+Go
+More
+1234567891011121314151617181920212223242526
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
+
+Line: 87 Col: 1
+
+Test against custom input
+
+*/
+
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
+ 
+func gradingStudents(grades []int32) []int32 {
+    for i:=0;i<len(grades);i++{
+        if grades[i] < 38{
+            continue
+        }else{
+            unit_place := (grades[i]%10)
+            if unit_place == 3 || unit_place == 8{
+                grades[i]+=2
+            }else if unit_place == 4 || unit_place == 9{
+                grades[i]+=1
+            }             
+        }
+    }
+    return grades
+}
+
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
+
+    gradesCount, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+    checkError(err)
+
+    var grades []int32
+
+    for i := 0; i < int(gradesCount); i++ {
+        gradesItemTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+        checkError(err)
+        gradesItem := int32(gradesItemTemp)
+        grades = append(grades, gradesItem)
+    }
+
+    result := gradingStudents(grades)
+
+    for i, resultItem := range result {
+        fmt.Fprintf(writer, "%d", resultItem)
+
+        if i != len(result) - 1 {
+            fmt.Fprintf(writer, "\n")
+        }
+    }
+
+    fmt.Fprintf(writer, "\n")
+
+    writer.Flush()
+}
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
