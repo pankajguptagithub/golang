9b36afc0fa4b96ee2aa2d9b491071bb28f39e203
pankajguptagithub <pankajguptagithub@users.noreply.github.com>
2024-04-02 15:59:11 +0530

    divisible_sum_pairs.go

---
+++ hkrrnk/divisible_sum_pairs.go
@@ -0,0 +1,46 @@
+/*
+Given an array of integers and a positive integer , determine the number of  pairs where  and  +  is divisible by .
+
+Example
+Three pairs meet the criteria:  and .
+
+Function Description
+
+Complete the divisibleSumPairs function in the editor below.
+
+divisibleSumPairs has the following parameter(s):
+
+int n: the length of array 
+int ar[n]: an array of integers
+int k: the integer divisor
+Returns
+- int: the number of pairs
+
+Input Format
+
+The first line contains  space-separated integers,  and .
+The second line contains  space-separated integers, each a value of .
+
+Constraints
+
+Sample Input
+
+STDIN           Function
+-----           --------
+6 3             n = 6, k = 3
+1 3 2 6 1 2     ar = [1, 3, 2, 6, 1, 2]
+Sample Output
+*/
+// Solution written by me with nested loops
+func divisibleSumPairs(n int32, k int32, ar []int32) int32 {
+    count := 0
+    for i:=0;i<len(ar);i++{
+        for j:=i+1;j<len(ar);j++{
+            if (ar[i] + ar[j])%k == 0{
+                count ++
+            }    
+        }    
+    }
+    fmt.Println("Total count is ",count)
+    return int32(count)
+}
